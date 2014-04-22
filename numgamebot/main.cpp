7f16641c07b282c5bf2c7f3a838b144a256b4c1c
Alex Nichol <unixpickle@gmail.com>
2014-04-21 20:53:11 -0400

    Initial Commit

---
+++ numgamebot/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  numgamebot
+//
+//  Created by Alex Nichol on 4/21/14.
+//  Copyright (c) 2014 Alex Nichol. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
